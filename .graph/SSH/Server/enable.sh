2d8d5e1662a251ab647b3a136b4bb6b3ebec872e
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-09-07 22:27:16 -0500

    Update enable.sh

--- .graph/SSH/Server/enable.sh
+++ .graph/SSH/Server/enable.sh
@@ -1,2 +1,3 @@
+# from: https://discussion.fedoraproject.org/t/using-ansible-in-fedora-toolbox-to-configure-local-system/1061/5
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
